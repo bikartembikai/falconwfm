3bb50200ee25f6f885efd965eae65de434b1475f
bikartembikai <m.amir.afham@gmail.com>
2025-12-06 16:58:13 +0800

    test

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'operation_manager', 'marketing_manager', 'facilitator'])->default('facilitator');
             $table->rememberToken();
             $table->timestamps();
         });
